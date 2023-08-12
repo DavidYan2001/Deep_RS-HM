c53bf1046260a7c45abdd860179647e6402d83e9
David Yan <139680304+DavidYan2001@users.noreply.github.com>
2023-08-12 13:56:46 +0800

    Update and rename README.md to README.m

--- README.m
+++ README.m
@@ -1,2 +1,4 @@
 # Deep_HM
 This is for the paper "Deep Homography Mixture for Single Image Rolling Shutter Correction" which is accepted in ICCV 2023. Our code will be released soon.
+
+The synthetic dataset RS-Homo can be generated in https://drive.google.com/drive/folders/1-03hg5S84E0GYJOBPB5USsqYj-Z0cXzY.
