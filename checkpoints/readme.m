fb06c92f89b8d90fee4371711011656cffab4c00
David Yan <139680304+DavidYan2001@users.noreply.github.com>
2023-09-14 13:09:39 +0800

    Update readme.m

--- checkpoints/readme.m
+++ checkpoints/readme.m
@@ -1,1 +1,2 @@
-
+The model param file can be found here: https://drive.google.com/file/d/15jHR0QP2qPd1w3mYJ03xeH7UxH0B1qKY/view?usp=drive_link.
+Noted that the model is for 8 blocks with 8 learned Homo-bases.
