5881d2dddc47f4fa85439191b9383354e9ae3094
Shuaicheng Liu <liuyangmao@gmail.com>
2023-10-26 12:03:33 +0800

    Update README.m

--- README.m
+++ README.m
@@ -1,5 +1,9 @@
-[ICCV 2023] Deep Homography Mixture for Single Image Rolling Shutter Correction
+## [ECCV 2022] Deep Homography Mixture for Single Image Rolling Shutter Correction
 
-This is for the paper "Deep Homography Mixture for Single Image Rolling Shutter Correction" which is accepted in ICCV 2023. Our code will be updated soon.
+<h4 align="center">Weilong Yan<sup>1</sup>, Robby T. Tan<sup>2</sup>, Bing Zeng<sup>1</sup>, Shuaicheng Liu*<sup>1</sup></center>
+<h4 align="center">1. University of Electronic Science and Technology of China</center>
+<h4 align="center">2. National University of Singapore</center><br><br>
+
+#### This is the official implementation of our ICCV 2023 paper "Deep Homography Mixture for Single Image Rolling Shutter Correction"
 
 The synthetic dataset RS-Homo can be generated in https://drive.google.com/drive/folders/1-03hg5S84E0GYJOBPB5USsqYj-Z0cXzY.
